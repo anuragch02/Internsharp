0a931cd9ca85cb0c80325a9cb8fe801e59dea821
kshama gupta <kshamagupta200@gmail.com>
2025-04-25 14:08:16 +0530

    Fronted of Login Page is Completed

---
+++ InternSharp/Controllers/AccountsController.cs
@@ -0,0 +1,30 @@
+﻿using InternSharp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InternSharp.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            if (model.Username == "admin" && model.Password == "admin123")
+            {
+                ViewBag.Message = "Login Successful!";
+            }
+            else
+            {
+                ViewBag.Message = "Invalid Credentials";
+            }
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
