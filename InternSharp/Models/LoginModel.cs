d1d8582778b986133156175f0d31240459aa2463
kshama gupta <kshamagupta200@gmail.com>
2025-04-28 09:51:10 +0530

    Login Backend Implementation

---
+++ InternSharp/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InternSharp.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [EmailAddress]
+        public string EmailID { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+}
