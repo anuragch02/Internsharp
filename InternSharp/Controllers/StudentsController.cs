da64dbc1150ebe4b8b629759611914b5d576555e
kshama gupta <kshamagupta200@gmail.com>
2025-05-02 10:48:18 +0530

    latest code

---
+++ InternSharp/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InternSharp.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Internship()
+        {
+            return View();
+        }
+        public IActionResult Jobs()
+        {
+            return View();
+        }
+        public IActionResult Courses()
+        {
+            return View();
+        }
+    }
+}
